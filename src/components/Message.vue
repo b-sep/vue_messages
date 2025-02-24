a4c4b2c27f748e9fdd81c3c200e703c6f97bc623
Júnior <b.sep@live.com>
2025-02-24 16:00:40 -0300

    message and messages container components

---
+++ src/components/Message.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+  defineProps<{ content: String, sended_at?: String, to?: String, from?: String, received_at?: String }>()
+</script>
+
+<template>
+  <li :class="sended_at ? 'sended' : 'received'">
+    <p>
+      {{ sended_at ? 'Para: ' : 'De: ' }}
+      <span>{{ sended_at ? to : from }}</span>
+    </p>
+    <p>
+      {{ content }}
+    </p>
+    <p class="hour">
+      {{ sended_at || received_at }}
+    </p>
+  </li>
+</template>
+
+<style scoped>
+  .hour {
+    font-style: italic;
+    font-weight: 300;
+    font-size: 0.8rem;
+  }
+
+  .sended {
+    background-color: lightgreen;
+  }
+
+  .received {
+    background-color: lightblue;
+  }
+
+  li {
+    border: 1px solid #bbb;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    line-height: 1.5rem;
+    padding: 0.5rem;
+    text-align: left;
+  }
+</style>
---
+++ src/components/MessagesContainer.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+  import type { Messages } from '../types/api'
+  import Message from './Message.vue';
+
+  defineProps<{
+    messages: Messages
+  }>()
+</script>
+
+<template>
+  <div>
+    <section class="message-container">
+      <h1>Mensagens Enviadas</h1>
+
+      <ul v-if="messages.sended_messages.length > 0">
+        <Message
+          v-for="(message, index) in messages.sended_messages"
+          :key="index"
+          :content="message.content"
+          :sended_at="message.sended_at"
+          :to="message.to"
+        />
+      </ul>
+      <p v-else>Nenhuma mensagem enviada :(</p>
+    </section>
+
+    <section class="message-container">
+      <h1>Mensagens Recebidas</h1>
+
+      <ul v-if="messages.received_messages.length > 0">
+        <Message
+          v-for="(message, index) in messages.received_messages"
+          :key="index"
+          :content="message.content"
+          :received_at="message.received_at"
+          :from="message.from"
+        />
+      </ul>
+      <p v-else>Nenhuma mensagem recebida :(</p>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+  .message-container {
+    height: 90vh;
+    padding: 1rem;
+    width: 700px;
+    text-align: center;
+  }
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 2rem;
+    padding: 1rem;
+  }
+
+  h1 {
+    margin-bottom: 2rem;
+  }
+
+  p {
+    color: #4E2862;
+    font-weight: 400;
+  }
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    border: 1px solid #bbb;
+    border-radius: 5px;
+    max-height: 70vh;
+    list-style: none;
+    padding: 1rem;
+    overflow-y: auto;
+  }
+</style>
